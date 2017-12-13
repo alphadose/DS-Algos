60a612b0a99ce07b89c44b76678a4f00f470d045
Alphadose <anish.mukherjee1996@gmail.com>
2017-12-13 08:54:02 +0530

    Solved Packaging Cupcakes

---
+++ practice/beginner/Packaging_Cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	long int t,n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		cout<<int(n/2)+1<<endl;
+	}
+	return 0;
+}
