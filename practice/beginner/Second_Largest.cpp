a787fc93468f6ce3cae275d335f78550853c34c0
Alphadose <anish.mukherjee1996@gmail.com>
2017-12-16 06:57:32 +0530

    Solved Second Largest

---
+++ practice/beginner/Second_Largest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int a[3];
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>a[0]>>a[1]>>a[2];
+		sort(a,a+3);
+		cout<<a[1]<<endl;
+	}
+	return 0;
+}
