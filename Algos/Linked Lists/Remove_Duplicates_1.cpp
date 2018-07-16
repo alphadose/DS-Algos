237fa15b71c65f695481028ad72dca84cd16680d
Alphadose <anish.mukherjee1996@gmail.com>
2018-07-16 10:02:25 +0530

    Solved Remove Duplicates 1

---
+++ Algos/Linked Lists/Remove_Duplicates_1.cpp
@@ -0,0 +1,30 @@
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    if(A == NULL){
+        return A;
+    }
+    
+    ListNode* curr;
+    ListNode* temp;
+    
+    curr = A;
+    
+    while(curr->next != NULL){
+        temp = curr->next;
+        while(curr->val == temp->val){
+            if(temp->next == NULL){
+                curr->next = NULL;
+                return A;
+            }
+            temp = temp->next;
+        }
+        curr->next = temp;
+        curr = temp;
+    }
+    
+    return A;
+}
