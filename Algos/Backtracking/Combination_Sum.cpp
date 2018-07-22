c3d163d95bc8326a8672f12b6cd35f9506ce49bd
Alphadose <anish.mukherjee1996@gmail.com>
2018-07-22 18:34:56 +0530

    Solved Combination Sum

---
+++ Algos/Backtracking/Combination_Sum.cpp
@@ -0,0 +1,23 @@
+bool solve(vector<int> A, vector<int> &temp, int B, int sum, vector<vector<int> > &ans) {
+    if(sum==B) return true;
+    if(sum>B) return false;
+    for(int i=0;i<A.size();i++) {
+        temp.push_back(A[i]);
+        sum+=A[i];
+        if(solve(A, temp, B, sum, ans))
+            if(sum==B)
+                ans.push_back(temp);
+        sum-=A[i];
+        temp.pop_back();
+    }
+}
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    vector<vector<int> > ans;
+    vector<int> temp;
+    solve(A, temp, B, 0, ans);
+    for(int i=0;i<ans.size();i++)
+        sort(ans[i].begin(),ans[i].end());
+    sort(ans.begin(),ans.end());
+    set<vector<int>> tr(ans.begin(),ans.end());
+    return vector<vector<int>> (tr.begin(),tr.end());
+}
