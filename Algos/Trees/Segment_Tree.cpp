211823f28e0a858368668e19249319a5559e1fa3
Alphadose <anish.mukherjee1996@gmail.com>
2018-08-25 05:05:49 +0530

    Added Lazy propgatation to segment tree

--- Algos/Trees/Segment_Tree.cpp
+++ Algos/Trees/Segment_Tree.cpp
@@ -28,7 +28,7 @@ void update(int node, int start, int end, int idx, int val)
     else
     {
         int mid = (start + end) / 2;
-        if(start <= idx and idx <= mid)
+        if(start <= idx && idx <= mid)
         {
             // If idx is in the left child, recurse on the left child
             update(2*node, start, mid, idx, val);
@@ -45,12 +45,12 @@ void update(int node, int start, int end, int idx, int val)
 
 int query(int node, int start, int end, int l, int r)
 {
-    if(r < start or end < l)
+    if(r < start || end < l)
     {
         // range represented by a node is completely outside the given range
         return 0;
     }
-    if(l <= start and end <= r)
+    if(l <= start && end <= r)
     {
         // range represented by a node is completely inside the given range
         return tree[node];
@@ -65,7 +65,7 @@ int query(int node, int start, int end, int l, int r)
 void updateRange(int node, int start, int end, int l, int r, int val)
 {
     // out of range
-    if (start > end or start > r or end < l)
+    if (start > end || start > r || end < l)
         return;
 
     // Current node is a leaf node
@@ -85,3 +85,60 @@ void updateRange(int node, int start, int end, int l, int r, int val)
     tree[node] = tree[node*2] + tree[node*2+1];
 }
 
+// Lazy Propagation
+
+void updateRange(int node, int start, int end, int l, int r, int val)
+{
+    if(lazy[node] != 0)
+    { 
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];    // Update it
+        if(start != end)
+        {
+            lazy[node*2] += lazy[node];                  // Mark child as lazy
+            lazy[node*2+1] += lazy[node];                // Mark child as lazy
+        }
+        lazy[node] = 0;                                  // Reset it
+    }
+    if(start > end || start > r || end < l)              // Current segment is not within range [l, r]
+        return;
+    if(start >= l && end <= r)
+    {
+        // Segment is fully within range
+        tree[node] += (end - start + 1) * val;
+        if(start != end)
+        {
+            // Not leaf node
+            lazy[node*2] += val;
+            lazy[node*2+1] += val;
+        }
+        return;
+    }
+    int mid = (start + end) / 2;
+    updateRange(node*2, start, mid, l, r, val);        // Updating left child
+    updateRange(node*2 + 1, mid + 1, end, l, r, val);   // Updating right child
+    tree[node] = tree[node*2] + tree[node*2+1];        // Updating root with max value 
+}
+
+int queryRange(int node, int start, int end, int l, int r)
+{
+    if(start > end || start > r || end < l)
+        return 0;         // Out of range
+    if(lazy[node] != 0)
+    {
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];            // Update it
+        if(start != end)
+        {
+            lazy[node*2] += lazy[node];         // Mark child as lazy
+            lazy[node*2+1] += lazy[node];    // Mark child as lazy
+        }
+        lazy[node] = 0;                 // Reset it
+    }
+    if(start >= l && end <= r)             // Current segment is totally within range [l, r]
+        return tree[node];
+    int mid = (start + end) / 2;
+    int p1 = queryRange(node*2, start, mid, l, r);         // Query left child
+    int p2 = queryRange(node*2 + 1, mid + 1, end, l, r); // Query right child
+    return (p1 + p2);
+}
